70e0df11fbb1f305e5360e42df00acf49dca46b7
tt225q <tt225q@gmail.com>
2023-02-19 01:47:45 +0800

    My DjangoGirls app, first commit

---
+++ .gitignore.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 01:39:08 2023
+
+@author: wangchungpin
+"""
+
+# Python
+*.pyc
+*~
+__pycache__
+
+# Env
+.env
+myvenv/
+venv/
+
+# Database
+db.sqlite3
+
+# Static folder at project root
+/static/
+
+# macOS
+._*
+.DS_Store
+.fseventsd
+.Spotlight-V100
